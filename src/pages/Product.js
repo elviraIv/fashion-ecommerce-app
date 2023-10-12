1a9e31e82a9adb7ed830737f1d27335312c0c132
Elvira <elvira.n.ivanova@gmail.com>
2023-10-12 14:48:47 +0300

    Added additional styling

--- src/pages/Product.js
+++ src/pages/Product.js
@@ -24,52 +24,111 @@ const InfoContainer = styled.div`
   padding: 0px 50px;
 `;
 const Title = styled.h1`
-font-weight: 200;
+  font-weight: 200;
 `;
 const Desc = styled.p`
-    margin: 20px 0px;
+  margin: 20px 0px;
 `;
 const Price = styled.span`
-    font-weight: 100;
-    font-size: 40px;
+  font-weight: 100;
+  font-size: 40px;
 `;
 
 const FilterContainer = styled.div`
-width: 50%;
-margin: 30px 0px;
-display: flex ;
-justify-content: space-between;
-
-`
-
+  width: 50%;
+  margin: 30px 0px;
+  display: flex;
+  justify-content: space-between;
+`;
 
 const Filter = styled.div`
-display: flex;
-align-items: center;
-
-`
+  display: flex;
+  align-items: center;
+`;
 
 const FilterTitle = styled.span`
-font-size:20px;
-font-weight: 200;
-`
-
+  font-size: 20px;
+  font-weight: 200;
+`;
 
 const FilterColor = styled.div`
-    width: 20px;
-    height: 20px;
-    border-radius: 50%;
-    background-color: ${props=> props.color} ;
-    margin: 0px 5px;
-    cursor: pointer;
-`
+  width: 20px;
+  height: 20px;
+  border-radius: 50%;
+  background-color: ${(props) => props.color};
+  margin: 0px 5px;
+  cursor: pointer;
+`;
 
 const FilterSize = styled.select`
-margin-left: 10px;
-padding: 8px;
-`
+  margin-left: 10px;
+  padding: 8px;
+`;
 
-const FilterSizeOption = styled.option``
+const FilterSizeOption = styled.option`
+
+`;
+
+const AddContainer = styled.div`
+    width: 50px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+`;
+
+const AmountContainer = styled.div`
+display: flex;
+align-items: center;
+font-weight: 700;
+
+`;
+
+const Amount = styled.span`
+width: 30px;
+height: 30px;
+   border-radius: 6px;
+   border: 1px solid teal;
+   display: flex;
+   align-items: center;
+   justify-content: center;
+   margin: 5px;
+`;
+
+const Button = styled.button`
+
+padding: 15px;
+border: 1px solid teal;
+background-color: white;
+cursor: pointer;
+display: flex;
+align-items: center;
+justify-content: center;
+background-color: aliceblue;
+margin-left: 100px;
+
+
+&:hover{
+    background-color: #f8f4f4;
+}
+
+`;
+
+const Add = styled.button`
+margin: 10px;
+background-color: white;
+border: none;
+font-size: 30px;
+cursor: pointer;
+`;
+
+const Remove = styled.button`
+margin: 10px;
+background-color: white;
+border: none;
+font-size: 40px;
+cursor: pointer;
+`;
 
 
 const Product = () => {
@@ -91,23 +150,31 @@ const Product = () => {
           </Desc>
           <Price>$ 299</Price>
           <FilterContainer>
-                <Filter>
-                    <FilterTitle>Color</FilterTitle>
-                    <FilterColor color = "black"/>
-                    <FilterColor color = "darkblue"/>
-                    <FilterColor color = "gray"/>
-                </Filter>
-                <Filter>
-                    <FilterTitle>Size</FilterTitle>
-                    <FilterSize>
-                        <FilterSizeOption>XS</FilterSizeOption>
-                        <FilterSizeOption>S</FilterSizeOption>
-                        <FilterSizeOption>M</FilterSizeOption>
-                        <FilterSizeOption>L</FilterSizeOption>
-                        <FilterSizeOption>XL</FilterSizeOption>
-                    </FilterSize>
-                </Filter>
+            <Filter>
+              <FilterTitle>Color</FilterTitle>
+              <FilterColor color="black" />
+              <FilterColor color="darkblue" />
+              <FilterColor color="gray" />
+            </Filter>
+            <Filter>
+              <FilterTitle>Size</FilterTitle>
+              <FilterSize>
+                <FilterSizeOption>XS</FilterSizeOption>
+                <FilterSizeOption>S</FilterSizeOption>
+                <FilterSizeOption>M</FilterSizeOption>
+                <FilterSizeOption>L</FilterSizeOption>
+                <FilterSizeOption>XL</FilterSizeOption>
+              </FilterSize>
+            </Filter>
           </FilterContainer>
+          <AddContainer>
+            <AmountContainer>
+              <Remove>-</Remove>
+              <Amount>1</Amount>
+              <Add>+</Add>
+            </AmountContainer>
+            <Button>ADD TO CART</Button>
+          </AddContainer>
         </InfoContainer>
       </Wrapper>
       <NewsLatter />
